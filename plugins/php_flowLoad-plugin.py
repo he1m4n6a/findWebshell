7da332c9102ac499348dac953cc61401d8936371
he1m4n6a <he1m4n6a@163.com>
2018-07-08 13:35:52 +0800

    Create php_flowLoad-plugin.py
    类转载器webshell

---
+++ plugins/php_flowLoad-plugin.py
@@ -0,0 +1,13 @@
+#/usr/bin/env python
+#coding=utf8
+
+import re
+
+rule='(include|require)(_once){0,1}[\s*]+[\"|\']+[0-9A-Za-z_]*\:// '
+
+def judgeBackdoor(fileCtent):
+	result = re.compile(rule).findall(fileCtent)
+	print result
+	if len(result) > 0:
+		return "流包装器加密后门"
+	return None
